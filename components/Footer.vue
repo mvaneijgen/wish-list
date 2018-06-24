acfdd91d199066dd3cd6af725f9d4a52d5e49757
Mitchel van Eijgen <mvaneijgen@gmail.com>
2018-06-24 10:09:54 +0200

    ran build command

--- components/Footer.vue
+++ components/Footer.vue
@@ -30,38 +30,38 @@ export default {
   // props: [],
   data() {
     return {
-      title: 'Footer',
+      title: "Footer",
       about:
         'Op de één of andere manier vinden mensen het lastig om iets voor mij als cadeau te bedenken, terwijl ik denk dat het super makkelijk is. Zo als je hier boven ook ziet is het thema "dingen die op kunnen". En ja ik wil iedere keer kaas en whiskey, want die dingen gaan op een gegeven moment op en heb ze nog nooit allemaal geprobeerd. Daarnaast heb ik een soort van hekel aan spullen, dus als je iets anders wil kiezen dan hier op deze lijst bedenk dan dat het of op moet kunne of een ervaring is (ergens naar toe, kaartje voor een escape room of VR gamen wil ik ook altijd nog proberen).',
       me: {
-        name: 'Mitchel van Eijgen',
-        website: 'mvaneijgen.nl',
-        email: 'mvaneijgen@gmail.com',
-        social: "Add me on 'any-social-network.com/mvaneijgen'",
+        name: "Mitchel van Eijgen",
+        website: "mvaneijgen.nl",
+        email: "mvaneijgen@gmail.com",
+        social: "Add me on 'any-social-network.com/mvaneijgen'"
       },
       tools: [
         {
-          name: 'Vue.js',
+          name: "Vue.js",
           description:
-            'In samenwerking met Nuxt.js word deze Vue.js site gecompiled naar een statische app.',
-          link: 'https://vuejs.org/v2/guide/installation.html',
+            "In samenwerking met Nuxt.js word deze Vue.js site gecompiled naar een statische app.",
+          link: "https://vuejs.org/v2/guide/installation.html"
         },
         {
-          name: 'Todoist',
+          name: "Todoist",
           description:
             'Op todoist beheer in een lijst met dingen die ik graag nog wil hebben en deze haal ik binnen op deze website <a href="https://developer.todoist.com/rest/v8/" class="link">via de API</a>.',
-          link: 'http://todoist.com',
+          link: "http://todoist.com"
         },
         {
-          name: 'Github pages',
+          name: "Github pages",
           description:
-            'Deze site word gehost op Github pages. Een gratis webhosting voor kleine projecten direct van uit je project reposetory.',
-          link: 'https://pages.github.com',
-        },
+            "Deze site word gehost op Github pages. Een gratis webhosting voor kleine projecten direct van uit je project reposetory.",
+          link: "https://pages.github.com"
+        }
       ],
-      toolsIntro: '',
+      toolsIntro: ""
     };
-  }, // End data
+  } // End data
   // computed: {},
   // methods: {},
   // watch: {},
@@ -73,26 +73,28 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import '~/assets/css/common/_variables.scss';
+@import "~/assets/css/common/_variables.scss";
 
 footer {
   color: $brand-light;
-
   h4 {
     margin-bottom: 0;
     font-size: 1.2em;
   }
-  ul, span, p {
+  ul,
+  span,
+  p {
     font-size: 1rem;
     line-height: 1.4em;
   }
+
   ul {
     list-style: none;
     padding: 0;
     margin: 0;
   }
   p {
-    margin-bottom: $base-margin*2;
+    margin-bottom: $base-margin * 2;
   }
 }
 </style>
