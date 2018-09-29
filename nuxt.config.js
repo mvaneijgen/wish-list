847cc1283f46c63420d9fe262831d39ee10aaffc
Mitchel van Eijgen <mvaneijgen@gmail.com>
2018-09-29 12:22:16 +0200

    Modified nuxt mode now a spa?

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
 const routerBase =
-  process.env.DEPLOY_ENV === 'GH_PAGES'
+  process.env.DEPLOY_ENV === "GH_PAGES"
     ? {
         router: {
-          base: '/wish-list/',
+          base: "/wish-list/",
         },
       }
     : {};
@@ -11,48 +11,55 @@ module.exports = {
   /*
   ** Headers of the page
   */
+  mode: "spa",
   router: {
     // base: '/wish-list/',
   },
+  build: {
+    dir: "docs",
+    // router: {
+    //   base: '/wish-list/',
+    // },
+  },
   generate: {
-    dir: 'docs',
+    dir: "docs",
     // router: {
     //   base: '/wish-list/',
     // },
   },
   ...routerBase,
-  css: ['@/assets/css/main.scss'],
+  css: ["@/assets/css/main.scss"],
   head: {
-    title: 'Wishlist app | M van Eijgen',
+    title: "Wishlist app | M van Eijgen",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
-      { property: 'og:type', content: 'article' },
-      { property: 'og:title', content: 'Wish list app | M van Eijgen' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" },
+      { property: "og:type", content: "article" },
+      { property: "og:title", content: "Wish list app | M van Eijgen" },
       {
-        property: 'og:description',
+        property: "og:description",
         content:
-          'Op de één of andere manier vinden mensen het lastig om iets voor mij als cadeau te bedenken, terwijl ik denk dat het super makkelijk is...',
+          "Op de één of andere manier vinden mensen het lastig om iets voor mij als cadeau te bedenken, terwijl ik denk dat het super makkelijk is...",
       },
-      { property: 'og:url', content: 'https://mvaneijgen.nl/wish-list' },
+      { property: "og:url", content: "https://mvaneijgen.nl/wish-list" },
       {
-        property: 'og:image',
-        content: 'https://mvaneijgen.nl/wish-list/social.jpg',
+        property: "og:image",
+        content: "https://mvaneijgen.nl/wish-list/social.jpg",
       },
     ],
     link: [
       {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: 'https://mvaneijgen.nl/wish-list/favicon.ico',
+        rel: "icon",
+        type: "image/x-icon",
+        href: "https://mvaneijgen.nl/wish-list/favicon.ico",
       },
     ],
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
@@ -63,9 +70,9 @@ module.exports = {
     extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/,
         });
       }
--- pages/index.vue
+++ pages/index.vue
@@ -23,105 +23,106 @@
 </template>
 
 <script>
-import Header from "~/components/Header.vue";
-import Footer from "~/components/Footer.vue";
+  import Header from "~/components/Header.vue";
+  import Footer from "~/components/Footer.vue";
 
-import Items from "~/components/Items.vue";
-import CurrentItem from "~/components/CurrentItem.vue";
+  import Items from "~/components/Items.vue";
+  import CurrentItem from "~/components/CurrentItem.vue";
 
-// import axios from 'axios';
-//
-// axios.defaults.headers.common['Authorization'] =
-//   'Bearer f5125aed0f383716e33f9dedf5715b9ace5c3825';
-// axios
-//   .get(`https://beta.todoist.com/API/v8/projects/2187923865`)
-//   .then(function(response) {
-//     console.log(response);
-//   })
-//   .catch(function(error) {
-//     console.log(error);
-//   });
+  // import axios from 'axios';
+  //
+  // axios.defaults.headers.common['Authorization'] =
+  //   'Bearer f5125aed0f383716e33f9dedf5715b9ace5c3825';
+  // axios
+  //   .get(`https://beta.todoist.com/API/v8/projects/2187923865`)
+  //   .then(function(response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function(error) {
+  //     console.log(error);
+  //   });
 
-export default {
-  data() {
-    return {};
-  },
-  metaInfo: {
-    title: "Default Title"
-  },
-  middleware: "api",
-  components: {
-    "alloy-items": Items,
-    "alloy-currentitem": CurrentItem,
-    "alloy-header": Header,
-    "alloy-footer": Footer
-  },
-  mounted() {
-    const elementPos = currentItem.offsetTop;
+  export default {
+    data() {
+      return {};
+    },
+    metaInfo: {
+      title: "Default Title"
+    },
+    middleware: "api",
+    components: {
+      "alloy-items": Items,
+      "alloy-currentitem": CurrentItem,
+      "alloy-header": Header,
+      "alloy-footer": Footer
+    },
+    mounted() {
+      const elementPos = currentItem.offsetTop;
 
-    window.addEventListener("scroll", event => {
-      // console.log(event);
-      const currentItem = document.querySelector(".alloy-min-height");
-      const main = document.querySelector("main");
-      const scrollPos =
-        window.pageYOffset || document.documentElement.scrollTop;
+      window.addEventListener("scroll", event => {
+        // console.log(event);
+        const currentItem = document.querySelector(".alloy-min-height");
+        const main = document.querySelector("main");
+        const scrollPos =
+          window.pageYOffset || document.documentElement.scrollTop;
 
-      const srollDownThreshold = scrollPos + 60;
-      console.log(srollDownThreshold + " " + elementPos);
+        const srollDownThreshold = scrollPos + 60;
+        console.log(srollDownThreshold + " " + elementPos);
 
-      if (srollDownThreshold >= elementPos) {
-        main.classList.add("fixed");
-      }
-      // const srollUpThreshold = scrollPos - 300;
-      // if (main.classList.contains('fixed')) {
-      // console.log(srollUpThreshold + ' ' + elementPos);
-      if (srollDownThreshold < elementPos) {
-        main.classList.remove("fixed");
-      }
-      // }
-    });
-  }, // Be sure all elements are drawn
+        if (srollDownThreshold >= elementPos) {
+          main.classList.add("fixed");
+        }
+        // const srollUpThreshold = scrollPos - 300;
+        // if (main.classList.contains('fixed')) {
+        // console.log(srollUpThreshold + ' ' + elementPos);
+        if (srollDownThreshold < elementPos) {
+          main.classList.remove("fixed");
+        }
+        // }
+      });
+    }, // Be sure all elements are drawn
 
-  methods: {}
-};
+    methods: {}
+  };
+
 </script>
 
 <style scoped lang="scss">
-@import "~/assets/css/common/_variables.scss";
+  @import "~/assets/css/common/_variables.scss";
 
-.flip-enter {
-  /*transform: rotateY(0deg);*/
-}
+  .flip-enter {
+    /*transform: rotateY(0deg);*/
+  }
 
-.flip-enter-active {
-  /* animation: flip-in  0.5s ease-out forwards; */
-  animation-name: flip-in;
-  animation-duration: 600ms;
-  animation-delay: 0s;
-  animation-timing-function: ease-out;
-  animation-direction: alternate;
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-  animation-play-state: running;
-}
+  .flip-enter-active {
+    /* animation: flip-in  0.5s ease-out forwards; */
+    animation-name: flip-in;
+    animation-duration: 600ms;
+    animation-delay: 0s;
+    animation-timing-function: ease-out;
+    animation-direction: alternate;
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+    animation-play-state: running;
+  }
 
-.flip-leave {
-  /*transform: rotateY(0deg);*/
-}
+  .flip-leave {
+    /*transform: rotateY(0deg);*/
+  }
 
-.flip-leave-active {
-  /* animation: flip-out 0.5s ease-out forwards; */
-  animation-name: flip-out;
-  animation-duration: 600ms;
-  animation-delay: 0s;
-  animation-timing-function: ease-out;
-  animation-direction: alternate;
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-  animation-play-state: running;
-}
+  .flip-leave-active {
+    /* animation: flip-out 0.5s ease-out forwards; */
+    animation-name: flip-out;
+    animation-duration: 600ms;
+    animation-delay: 0s;
+    animation-timing-function: ease-out;
+    animation-direction: alternate;
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+    animation-play-state: running;
+  }
 
-/* @keyframes flip-out {
+  /* @keyframes flip-out {
   from {
     transform: rotateY(0deg);
   }
@@ -139,45 +140,45 @@ export default {
   }
 } */
 
-@keyframes flip-out {
-  0% {
-    transform: rotateY(0deg);
-    box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
+  @keyframes flip-out {
+    0% {
+      transform: rotateY(0deg);
+      box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
+    }
+    30% {
+      transform: rotateY(0deg) translateY(-20px);
+      box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
+    }
+    60% {
+      transform: rotateY(90deg) translateY(-20px);
+      box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
+    }
+    100% {
+      transform: rotateY(90deg) translateY(0);
+      box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
+    }
   }
-  30% {
-    transform: rotateY(0deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  60% {
-    transform: rotateY(90deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  100% {
-    transform: rotateY(90deg) translateY(0);
-    box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-}
 
-@keyframes flip-in {
-  0% {
-    transform: rotateY(90deg);
-    box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
+  @keyframes flip-in {
+    0% {
+      transform: rotateY(90deg);
+      box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
+    }
+    30% {
+      transform: rotateY(90deg) translateY(-20px);
+      box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
+    }
+    60% {
+      transform: rotateY(0deg) translateY(-20px);
+      box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
+    }
+    100% {
+      transform: rotateY(0deg) translateY(0);
+      box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
+    }
   }
-  30% {
-    transform: rotateY(90deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  60% {
-    transform: rotateY(0deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  100% {
-    transform: rotateY(0deg) translateY(0);
-    box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-}
 
-/* /* Heavy flips
+  /* /* Heavy flips
 /* @keyframes flip-out {
   0% {
     transform: rotateY(0deg);
@@ -247,4 +248,5 @@ export default {
     box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
   }
 } */
+
 </style>
