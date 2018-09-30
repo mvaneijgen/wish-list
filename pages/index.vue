8591bcbdd6043c84b5ccfbe32f1f6f407aa4b518
Mitchel van Eijgen <mvaneijgen@gmail.com>
2018-09-30 15:11:46 +0200

    Add staggerd css transtion

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
   <div>
 
     <alloy-header></alloy-header>
+    <div class="spinner" v-if="!loaded"></div>
 
     <main>
-
       <section class="alloy-min-height">
         <transition name="flip" mode="out-in">
           <alloy-currentitem :item="$store.state.currentItem" :key="$store.state.currentItem.id"></alloy-currentitem>
@@ -12,11 +12,12 @@
       </section>
 
       <section>
-        <alloy-items v-for="item in $store.state.items" :key="item.id" :item="item"></alloy-items>
+        <transition-group name="pop">
+          <alloy-items v-for="item in $store.state.items" :key="item.id" :item="item"></alloy-items>
+        </transition-group>
       </section>
 
     </main>
-
     <alloy-footer></alloy-footer>
 
   </div>
@@ -29,25 +30,14 @@
   import Items from "~/components/Items.vue";
   import CurrentItem from "~/components/CurrentItem.vue";
 
-  // import axios from 'axios';
-  //
-  // axios.defaults.headers.common['Authorization'] =
-  //   'Bearer f5125aed0f383716e33f9dedf5715b9ace5c3825';
-  // axios
-  //   .get(`https://beta.todoist.com/API/v8/projects/2187923865`)
-  //   .then(function(response) {
-  //     console.log(response);
-  //   })
-  //   .catch(function(error) {
-  //     console.log(error);
-  //   });
-
   export default {
     data() {
-      return {};
+      return {
+        loaded: false,
+      };
     },
     metaInfo: {
-      title: "Default Title"
+      title: "Wish List"
     },
     // middleware: "api",
     components: {
@@ -87,7 +77,8 @@
       await this.$axios
         .get('https://beta.todoist.com/API/v8/tasks?project_id=2187923865')
         .then(response => {
-          console.log(response);
+          // console.log(response);
+          this.loaded = true;
           const allItems = response.data;
 
           let resultItems = [];
@@ -152,24 +143,6 @@
   animation-play-state: running;
 }
 
-/* @keyframes flip-out {
-  from {
-    transform: rotateY(0deg);
-  }
-  to {
-    transform: rotateY(90deg);
-  }
-}
-
-@keyframes flip-in {
-  from {
-    transform: rotateY(90deg);
-  }
-  to {
-    transform: rotateY(0deg);
-  }
-} */
-
 @keyframes flip-out {
   0% {
     transform: rotateY(0deg);
@@ -207,75 +180,4 @@
     box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
   }
 }
-
-/* /* Heavy flips
-/* @keyframes flip-out {
-  0% {
-    transform: rotateY(0deg);
-    box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  30% {
-    transform: rotateY(0deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  50% {
-    transform: rotateY(90deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  70% {
-    transform: rotateY(0deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  50% {
-    transform: rotateY(90deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  60% {
-    transform: rotateY(0deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  75% {
-    transform: rotateY(90deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  100% {
-    transform: rotateY(90deg) translateY(0);
-    box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-}
-
-@keyframes flip-in {
-  0% {
-    transform: rotateY(90deg);
-    box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  30% {
-    transform: rotateY(90deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  50% {
-    transform: rotateY(0deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  60% {
-    transform: rotateY(90deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  /* 50% {
-    transform: rotateY(0deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  60% {
-    transform: rotateY(90deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  75% {
-    transform: rotateY(0deg) translateY(-20px);
-    box-shadow: 5px 25px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-  100% {
-    transform: rotateY(0deg) translateY(0);
-    box-shadow: 5px 5px 30px 0 rgba($brand-dark, $base-alpha/2);
-  }
-} */
 </style>
